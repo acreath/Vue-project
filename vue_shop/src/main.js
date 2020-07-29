a22242e46b5a61a543f823ea080d124db53fc450
skyone <skyone@skyonedeMacBook-Pro.local>
2020-07-29 19:59:09 +0800

    实现登录后续操作，存储token到sessionStorage

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
 
+import './assets/css/global.css'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+
 
 new Vue({
   router,
