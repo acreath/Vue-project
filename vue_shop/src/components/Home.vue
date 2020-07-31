8a7192622a80a7222e9a7b5d4cf06006d85c6008
skyone <skyone@skyonedeMacBook-Pro.local>
2020-07-31 20:27:37 +0800

    实现了主页的基本布局

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        <el-button type='info' @click="logout">退出</el-button>
-    </div>
+
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>Header <el-button type='info' @click="logout">退出</el-button></el-header>
+        
+        <!-- 页面主体区域 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">Aside</el-aside>
+            
+            <!-- 右侧内容主体 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+    
 </template>
 
 <script>
 export default {
     methods:{
         logout() {
+            //清空token，返回到登录页
             window.sessionStorage.clear()
             this.$router.push('/login')
         }
@@ -16,5 +29,21 @@ export default {
 </script>
 
 <style lang="less" scoped>
+    .home-container {
+        height: 100%;
+    }
+    
+    .el-header {
+        background-color: #373D41;
 
+    }
+    .el-aside {
+        background-color: #333744;
+    }
+
+    .el-main {
+        background-color: #eaedf1;
+    }
+
+    
 </style>
--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message,Header, Container, Aside, Main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Main)
+Vue.use(Aside)
+
 //弹框组件需要全局挂载
 Vue.prototype.$message = Message
